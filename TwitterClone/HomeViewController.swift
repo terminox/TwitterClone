ad8f4230b0545398667f6d65e710ca3acf78c4f9
Yossa Bourne <cybercanonx@gmail.com>
2019-08-11 00:53:16 +0700

    Add tweets.json as sample tweets

---
+++ TwitterClone/HomeViewController.swift
@@ -0,0 +1,16 @@
+//
+//  HomeViewController.swift
+//  TwitterClone
+//
+//  Created by Yossa Bourne on 8/11/19.
+//  Copyright © 2019 Yossa Bourne. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
