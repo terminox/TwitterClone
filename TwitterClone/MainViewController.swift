0d9d717980b8b19043454ed3d30f9285d1319c73
Yossa Bourne <cybercanonx@gmail.com>
2019-08-11 00:06:14 +0700

    - Remove entry to storyboard and default view controller - Add MainViewController

---
+++ TwitterClone/MainViewController.swift
@@ -0,0 +1,17 @@
+//
+//  MainViewController.swift
+//  TwitterClone
+//
+//  Created by Yossa Bourne on 8/11/19.
+//  Copyright © 2019 Yossa Bourne. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .blue
+    }
+}
