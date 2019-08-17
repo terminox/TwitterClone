e61a1c4ac1b7ba62880df870de42adad2f40c2d5
Yossa Bourne <cybercanonx@gmail.com>
2019-08-17 15:32:57 +0700

    - Add MenuViewController - Add Menu group

--- TwitterClone/AppDelegate.swift
+++ TwitterClone/AppDelegate.swift
@@ -16,7 +16,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         let window = UIWindow(frame: UIScreen.main.bounds)
-        window.rootViewController = UINavigationController(rootViewController: HomeViewController())
+        let vc = MenuViewController()
+        window.rootViewController = vc
         self.window = window
         window.makeKeyAndVisible()
         return true
---
+++ TwitterClone/Scenes/Menu/MenuViewController.swift
@@ -0,0 +1,17 @@
+//
+//  MenuViewController.swift
+//  TwitterClone
+//
+//  Created by Yossa Bourne on 8/17/19.
+//  Copyright © 2019 Yossa Bourne. All rights reserved.
+//
+
+import UIKit
+
+class MenuViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .blue
+    }
+}
